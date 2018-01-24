313dbb730a079cb617c36ae7836f0a79c6b2ef44
David Hewitt <david.hewitt@indigotree.co.uk>
2018-01-24 16:45:40 +0000

    fixed environment variables

--- algolia-push.sh
+++ algolia-push.sh
@@ -2,4 +2,4 @@ curl -X POST \
     -H "X-Algolia-API-Key: ${ALGOLIA_API_KEY}" \
     -H "X-Algolia-Application-Id: ${ALGOLIA_APPLICATION_ID}" \
     --data-binary @public/index.algolia.json \
-    "https://${ALGOLIA_APPLICATION_ID}.algolia.net/1/indexes/${ALGOLIA_INDEX_NAME}/batch"
+    "https://${ALGOLIA_APPLICATION_ID}.algolia.net/1/indexes/${ALGOLIA_INDEX}/batch"
