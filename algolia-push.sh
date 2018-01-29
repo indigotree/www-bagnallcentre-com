93f446db02d45151334b3a609b7e49fb1b4dcd78
David Hewitt <david.hewitt@indigotree.co.uk>
2018-01-29 10:22:34 +0000

    Clear indices then push up new indices

--- algolia-push.sh
+++ algolia-push.sh
@@ -1,5 +1,10 @@
 curl -X POST \
     -H "X-Algolia-API-Key: ${ALGOLIA_API_KEY}" \
     -H "X-Algolia-Application-Id: ${ALGOLIA_APPLICATION_ID}" \
+    "https://${ALGOLIA_APPLICATION_ID}.algolia.net/1/indexes/${ALGOLIA_INDEX}/clear"
+
+curl -X POST \
+    -H "X-Algolia-API-Key: ${ALGOLIA_API_KEY}" \
+    -H "X-Algolia-Application-Id: ${ALGOLIA_APPLICATION_ID}" \
     --data-binary @public/index.algolia.json \
     "https://${ALGOLIA_APPLICATION_ID}.algolia.net/1/indexes/${ALGOLIA_INDEX}/batch"
