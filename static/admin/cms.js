70952b0899291a0c72f012c37f82fe67e129986f
David Hewitt <david.hewitt@indigotree.co.uk>
2018-03-14 16:08:22 +0000

    Set an obvious date

--- static/admin/cms.js
+++ static/admin/cms.js
@@ -65,8 +65,13 @@ var TimeControl = createClass({
 
     handleChange: function () {
         var todayDate = moment({
+            year: 2000,
+            month: 1,
+            day: 1,
             hour: this.state.date.hour(),
-            minute: this.state.date.minutes()
+            minute: this.state.date.minutes(),
+            second: 000,
+            millisecond: 000
         })
         this.props.onChange(todayDate.format(moment.defaultFormat))
     },
