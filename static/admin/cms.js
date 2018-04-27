ef272e21ac0b72a077b2f1d75c5bad184c0157e0
David Hewitt <david.hewitt@indigotree.co.uk>
2018-04-27 14:39:10 +0100

    Update CMS required fields and added Yoast SEO to various

--- static/admin/cms.js
+++ static/admin/cms.js
@@ -162,7 +162,70 @@ var CategoriesControl = createClass({
 
 CMS.registerPreviewStyle('https://unpkg.com/netlify-cms-yoast-seo@~1.0/dist/main.css');
 
+// Pages
+//
 CMS.registerPreviewTemplate('pages', createClass({
+    render: function () {
+        const entry = this.props.entry
+        const title = entry.getIn(['data', 'title']) || ''
+
+        YOAST.setContent({
+            title: title,
+            description: entry.getIn(['data', 'description']) || '',
+            keyword: entry.getIn(['data', 'yoast_keyword']) || '',
+            text: entry.getIn(['data', 'body']) || '',
+            titleWidth: title.split('').length * 5 // 5px is an average width of each character ;)
+        })
+
+        return h('div', {},
+            this.props.widgetFor('body'),
+            YOAST.getScoresAsHTML(h)
+        );
+    }
+}));
+
+
+CMS.registerPreviewTemplate('news', createClass({
+    render: function () {
+        const entry = this.props.entry
+        const title = entry.getIn(['data', 'title']) || ''
+
+        YOAST.setContent({
+            title: title,
+            description: entry.getIn(['data', 'description']) || '',
+            keyword: entry.getIn(['data', 'yoast_keyword']) || '',
+            text: entry.getIn(['data', 'body']) || '',
+            titleWidth: title.split('').length * 5 // 5px is an average width of each character ;)
+        })
+
+        return h('div', {},
+            this.props.widgetFor('body'),
+            YOAST.getScoresAsHTML(h)
+        );
+    }
+}));
+
+CMS.registerPreviewTemplate('therapies', createClass({
+    render: function () {
+        const entry = this.props.entry
+        const title = entry.getIn(['data', 'title']) || ''
+
+        YOAST.setContent({
+            title: title,
+            description: entry.getIn(['data', 'description']) || '',
+            keyword: entry.getIn(['data', 'yoast_keyword']) || '',
+            text: entry.getIn(['data', 'body']) || '',
+            titleWidth: title.split('').length * 5 // 5px is an average width of each character ;)
+        })
+
+        return h('div', {},
+            this.props.widgetFor('body'),
+            YOAST.getScoresAsHTML(h)
+        );
+    }
+}));
+
+CMS.registerPreviewTemplate('whats-on', createClass({
     render: function () {
         const entry = this.props.entry
         const title = entry.getIn(['data', 'title']) || ''
